576488df82d987dd09d14d926de302f27faec0c1
Aleksizz-ctrl <Borovikovslava07@gmail.com>
2026-09-19 17:09:33 +0500

    created windowscheckbox

---
+++ src/com/astanait/gui/windows/WindowsCheckbox.java
@@ -0,0 +1,8 @@
+package com.astanait.gui.windows;
+import com.astanait.gui.Checkbox;
+public class WindowsCheckbox implements Checkbox {
+    @Override
+    public void paint() {
+        System.out.println("Rendering Windows checkbox");
+    }
+}
