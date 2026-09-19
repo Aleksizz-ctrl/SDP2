1252099d146a77724a6331aeca2ba5207c1a2d56
Aleksizz-ctrl <Borovikovslava07@gmail.com>
2026-09-19 17:19:50 +0500

    created macoscheckbox

---
+++ src/com/astanait/gui/macos/MacOSCheckbox.java
@@ -0,0 +1,8 @@
+package com.astanait.gui.macos;
+import com.astanait.gui.Checkbox;
+public class MacOSCheckbox implements Checkbox {
+    @Override
+    public void paint() {
+        System.out.println("Rendering macOS checkbox");
+    }
+}
