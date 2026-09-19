e0af43181625a2812610f7fb768390553fa1f2f6
Aleksizz-ctrl <Borovikovslava07@gmail.com>
2026-09-19 17:07:04 +0500

    created windowsbutton

---
+++ src/com/astanait/gui/windows/WindowsButton.java
@@ -0,0 +1,8 @@
+package com.astanait.gui.windows;
+import com.astanait.gui.Button;
+public class WindowsButton implements Button {
+    @Override
+    public void paint() {
+        System.out.println("Rendering Windows button");
+    }
+}
