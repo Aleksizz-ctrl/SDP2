edd7d44239f2f1175a9299bd7e33f719573f09cd
Aleksizz-ctrl <Borovikovslava07@gmail.com>
2026-09-19 11:42:18 +0500

    created ship

---
+++ src/com/astanait/logistics/transport/Ship.java
@@ -0,0 +1,7 @@
+package com.astanait.logistics.transport;
+public class Ship implements Transport {
+    @Override
+    public void deliver(String cargo, String destination) {
+        System.out.println("Ship delivers " + cargo + " to " + destination + " by sea");
+    }
+}
